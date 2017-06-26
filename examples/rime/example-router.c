8d92597f60095d99ba9bd8274ee88e59ef1ee0dc
Ekawahyu Susilo <ekawahyu@aisenke.com>
2017-06-26 02:04:51 -0700

    added multihop ping with routing header

--- examples/rime/example-router.c
+++ examples/rime/example-router.c
@@ -47,7 +47,7 @@
 
 #include <stdio.h>
 
-#define DEBUG 0
+#define DEBUG 1
 
 #if DEBUG
 #define PRINTF(...) printf(__VA_ARGS__)
@@ -76,6 +76,8 @@ enum {
   CONECTRIC_POLL_SENSORS_REPLY,
   CONECTRIC_POLL_NEIGHBORS,
   CONECTRIC_POLL_NEIGHBORS_REPLY,
+  CONECTRIC_MULTIHOP_PING,
+  CONECTRIC_MULTIHOP_PING_REPLY,
   CONECTRIC_ATTR_MAX
 };
 
@@ -264,16 +266,21 @@ multihop_forward(struct multihop_conn *c,
   const linkaddr_t *originator, const linkaddr_t *dest,
   const linkaddr_t *prevhop, uint8_t hops)
 {
-  linkaddr_t msender, mreceiver;
+  linkaddr_t sender, msender, mreceiver;
   uint8_t message[128];
-  uint8_t mhops, packetlen, hdrlen;
+  uint8_t mhops, packetlen, hdrlen, datalen;
   uint8_t * header;
+  uint8_t * hdrptr;
+  uint8_t * dataptr;
+  uint8_t request;
+  uint8_t with_routing_table;
   int i;
 
   /* Copy the packet attributes to avoid them being overwritten or
    * cleared by an application program that uses the packet buffer for
    * its own needs
    */
+  linkaddr_copy(&sender, packetbuf_addr(PACKETBUF_ADDR_SENDER));
   linkaddr_copy(&msender, packetbuf_addr(PACKETBUF_ADDR_ESENDER));
   linkaddr_copy(&mreceiver, packetbuf_addr(PACKETBUF_ADDR_ERECEIVER));
   mhops = packetbuf_attr(PACKETBUF_ATTR_HOPS);
@@ -292,23 +299,48 @@ multihop_forward(struct multihop_conn *c,
   memset(message, 0, sizeof(message));
   packetlen = packetbuf_copyto(message);
 
-  /* Discard current packet header */
+  /* Decoding message */
+  hdrptr = &message[0];
   hdrlen = message[0];
+  dataptr = &message[0] + hdrlen;
+  datalen = message[hdrlen];
+  request = *++dataptr;
+
+  /* Discard current packet header */
   packetbuf_copyfrom(&message[hdrlen], packetlen - hdrlen);
 
-  /* Add new packet header */
-  packetbuf_hdralloc(hdrlen + 2);
-  header = (uint8_t *)packetbuf_hdrptr();
-  *header++ = hdrlen + 2;
-  *header++ = message[1];
-  *header++ = message[2] + 1;
-  *header++ = message[3] + 1;
-  *header++ = message[4];
-  *header++ = message[5];
-  *header++ = linkaddr_node_addr.u8[1];
-  *header++ = linkaddr_node_addr.u8[0];
-  for (i = 6; i < hdrlen; i++) {
-    *header++ = message[i];
+  if (request == CONECTRIC_MULTIHOP_PING) {
+    /* Add new packet header */
+    packetbuf_hdralloc(hdrlen);
+    header = (uint8_t *)packetbuf_hdrptr();
+    *header++ = hdrlen;
+    *header++ = message[1];
+    *header++ = mhops;
+    *header++ = message[3];
+    *header++ = message[4];
+    *header++ = message[5];
+    for (i = 6; i < hdrlen; i++) {
+      *header++ = message[i];
+    }
+    forward_addr.u8[1] = message[4 + (mhops << 2)];
+    forward_addr.u8[0] = message[5 + (mhops << 2)];
+  }
+
+  if (request == CONECTRIC_ROUTE_REPLY) {
+    /* Add new packet header */
+    packetbuf_hdralloc(hdrlen + 2);
+    header = (uint8_t *)packetbuf_hdrptr();
+    *header++ = hdrlen + 2;
+    *header++ = message[1];
+    *header++ = message[2] + 1;
+    *header++ = message[3] + 1;
+    *header++ = message[4];
+    *header++ = message[5];
+    *header++ = linkaddr_node_addr.u8[1];
+    *header++ = linkaddr_node_addr.u8[0];
+    for (i = 6; i < hdrlen; i++) {
+      *header++ = message[i];
+    }
   }
 
   packetbuf_set_addr(PACKETBUF_ADDR_ESENDER, &msender);
@@ -437,6 +469,7 @@ PROCESS_THREAD(example_multihop_process, ev, data)
   static uint8_t message[128];
   static uint8_t message_len;
   static uint8_t * header;
+  static uint8_t routing_len;
   static uint8_t counter = 0;
   static uint8_t * request;
   static uint8_t reply;
@@ -456,63 +489,86 @@ PROCESS_THREAD(example_multihop_process, ev, data)
     request = (uint8_t *)data;
     memset(message, 0, sizeof(message));
 
-    /* if (*request == '<') then it's from serial port, do as follow */
-    //request++; /* skip the '<' */
-    //message[0] = *request++; /* message_len */
-    //message[1] = *request++; /* bytereq */
+    /* request comes from serial port */
+    if (*request == '<') {
 
-    /*
-     * TODO handler for request coming from serial port
-     */
+      request++; /* skip the '<' */
+      message[0] = *request++; /* message_len */
+      message[1] = *request++; /* bytereq */
 
-    /* else */
-    message[0] = *request++; /* message_len */
-    message[1] = *request++; /* bytereq */
+      /* Set the Rime address of the final receiver */
+      esender_addr.u8[1] = *request++;
+      esender_addr.u8[0] = *request++;
+      to.u8[1] = esender_addr.u8[1];
+      to.u8[0] = esender_addr.u8[0];
 
-    /* Set the Rime address of the final receiver */
-    to.u8[0] = esender_addr.u8[0];
-    to.u8[1] = esender_addr.u8[1];
+      if (message[1] == CONECTRIC_MULTIHOP_PING) {
+        routing_len = message[0] - 4;
+        message_len = 2;
+      }
 
-    if (message[1] == CONECTRIC_ROUTE_REQUEST) {
-      reply = CONECTRIC_ROUTE_REPLY;
-      message_len = 2;
-      /* TODO delay count (for now) 1 seconds for (local) trickle to subside */
-      etimer_set(&et, CLOCK_SECOND);
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      message[0] = message_len;
+      packetbuf_copyfrom(message, message_len);
+
+      packetbuf_hdralloc(6 + routing_len);
+
+      header = (uint8_t *)packetbuf_hdrptr();
+      *header++ = 6;            /* header len */
+      *header++ = counter++;    /* seqno */
+      *header++ = 0;            /* hop count */
+      *header++ = 0;            /* number of hops */
+      *header++ = to.u8[1];     /* destination addr H */
+      *header++ = to.u8[0];     /* destination addr L */
+      while(routing_len--)
+        *header++ = *request++;  /* routing table */
     }
-    if (message[1] == CONECTRIC_ROUTE_REQUEST_BY_SN) {
-      reply = CONECTRIC_ROUTE_REPLY;
-      message_len = 2;
-      /* TODO delay count (for now) 1 seconds for (local) trickle to subside */
-      etimer_set(&et, CLOCK_SECOND);
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    /* request comes from radio */
+    else {
+
+      message[0] = *request++; /* message_len */
+      message[1] = *request++; /* bytereq */
+
+      /* Set the Rime address of the final receiver */
+      to.u8[0] = esender_addr.u8[0];
+      to.u8[1] = esender_addr.u8[1];
+
+      if (message[1] == CONECTRIC_ROUTE_REQUEST) {
+        reply = CONECTRIC_ROUTE_REPLY;
+        message_len = 2;
+        /* TODO delay count (for now) 1 seconds for (local) trickle to subside */
+        etimer_set(&et, CLOCK_SECOND);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      }
+      if (message[1] == CONECTRIC_ROUTE_REQUEST_BY_SN) {
+        reply = CONECTRIC_ROUTE_REPLY;
+        message_len = 2;
+        /* TODO delay count (for now) 1 seconds for (local) trickle to subside */
+        etimer_set(&et, CLOCK_SECOND);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      }
+
+      message[0] = message_len;
+      message[1] = reply;
+      packetbuf_copyfrom(message, message_len);
+
+      packetbuf_hdralloc(6);
+
+      header = (uint8_t *)packetbuf_hdrptr();
+      *header++ = 6;          /* header len */
+      *header++ = counter++;  /* seqno */
+      *header++ = 0;          /* hop count */
+      *header++ = 0;          /* number of hops */
+      *header++ = to.u8[1];   /* destination addr H */
+      *header++ = to.u8[0];   /* destination addr L */
     }
-
-    message[0] = message_len;
-    message[1] = reply;
-    packetbuf_copyfrom(message, message_len);
-
-    packetbuf_hdralloc(6);
-
-    header = (uint8_t *)packetbuf_hdrptr();
-    *header++ = 6;          /* header len */
-    *header++ = counter++;  /* seqno */
-    *header++ = 0;          /* hop count */
-    *header++ = 0;          /* number of hops */
-    *header++ = to.u8[1];   /* destination addr H */
-    *header++ = to.u8[0];   /* destination addr L */
 
     /* Send the packet. */
     multihop_send(&multihop, &to);
 
-#if DEBUG
-    if (reply == CONECTRIC_ROUTE_REPLY) {
-      PRINTF("%d.%d: replying route request to %d.%d - %lu\n",
-          linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
-          to.u8[0], to.u8[1], clock_seconds());
-    }
-#endif
-
+    PRINTF("%d.%d: multihop send to %d.%d - %lu\n",
+        linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+        to.u8[0], to.u8[1], clock_seconds());
   }
 
   PROCESS_END();
@@ -523,7 +579,7 @@ PROCESS_THREAD(serial_in_process, ev, data)
   static uint8_t * request;
   static uint8_t counter;
   static uint8_t hex_string[2];
-  static uint8_t bytereq[32];
+  static uint8_t bytereq[128];
 
   PROCESS_BEGIN();
 
@@ -583,6 +639,8 @@ PROCESS_THREAD(serial_in_process, ev, data)
       if (bytereq[2] == CONECTRIC_ROUTE_REQUEST ||
           bytereq[2] == CONECTRIC_ROUTE_REQUEST_BY_SN)
         process_post(&example_trickle_process, PROCESS_EVENT_CONTINUE, bytereq);
+      if (bytereq[2] == CONECTRIC_MULTIHOP_PING)
+        process_post(&example_multihop_process, PROCESS_EVENT_CONTINUE, bytereq);
       else
         /* unknown request */
         PRINTF("%d.%d: Unknown request - %lu\n",
