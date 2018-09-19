b4890d27c0683b82775d7d0ea6cfdd95b1ddc515
Ekawahyu Susilo <ekawahyu.susilo@conectric.com>
2018-09-19 16:42:02 -0700

    added initial rht-probe for 2-way communication

---
+++ examples/conectric/rime-rht/rht-probe.c
@@ -0,0 +1,296 @@
+/*
+ * rht-probe.c
+ *
+ * Created on: Sep 18, 2018
+ *     Author: Ekawahyu Susilo
+ *
+ * Copyright (c) 2018, Conectric Network, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the copyright holder.
+ *
+ */
+
+/* General */
+#include <stdio.h>
+
+/* Contiki */
+#include "contiki.h"
+#include "debug.h"
+#include "net/rime/rime.h"
+#include "net/rime/conectric.h"
+#include "net/netstack.h"
+#include "random.h"
+
+/* Conectric Device */
+//#include "flash-logging.h"
+#include "dev/sht21/sht21-sensor.h"
+#include "dev/adc-sensor.h"
+#include "dev/serial-line.h"
+
+/* Conectric Network */
+#include "../command.h"
+#include "../conectric-messages.h"
+
+#define DEBUG 0
+#if DEBUG
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
+/* RHT Network Parameters */
+#define RHT_REPORTING_PERIOD    (59U * CLOCK_SECOND)
+#define RHT_HEADER_SIZE         6
+#define RHT_BOOT_PAYLOAD_SIZE   4
+#define RHT_PAYLOAD_SIZE        7
+static uint8_t message[CONECTRIC_MESSAGE_LENGTH];
+extern volatile uint16_t deep_sleep_requested;
+
+/* Flash Logging */
+//static uint8_t logData[4]= { 0x00, 0x00, 0x00, 0x00};
+
+/* Logging reference time every 12 hours */
+//#define LOGGING_REF_TIME_PD ((clock_time_t)(12U * CLOCK_SECOND * 60U * 60U))
+//enum
+//{
+//  RHT_RESERVED = 0x00,    // reserved
+//  RHT_SEND     = 0x01,    // send data event
+//};
+
+/*---------------------------------------------------------------------------*/
+struct conectric_conn conectric;
+/*---------------------------------------------------------------------------*/
+PROCESS(rht_conectric_process, "RS485 Conectric");
+PROCESS(serial_in_process, "SerialIn");
+AUTOSTART_PROCESSES(
+    &rht_conectric_process,
+//    &flash_log_process,
+    &serial_in_process
+);
+/*---------------------------------------------------------------------------*/
+static void
+sent(struct conectric_conn *c)
+{
+  PRINTF("packet sent\n");
+}
+
+static void
+recv(struct conectric_conn *c, const linkaddr_t *from, uint8_t hops)
+{
+  PRINTF("%d.%d: data from %d.%d len %d hops %d\n",
+      linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+      from->u8[0], from->u8[1], packetbuf_datalen(), hops);
+}
+
+static void
+localbroadcast(struct conectric_conn *c, const linkaddr_t *from)
+{
+  PRINTF("%d.%d: localbc from %d.%d: len %d\n",
+      linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+      from->u8[0], from->u8[1], packetbuf_datalen());
+}
+
+static void
+netbroadcast(struct conectric_conn *c, const linkaddr_t *from, uint8_t hops)
+{
+  PRINTF("%d.%d: netbc from %d.%d: len %d hops %d\n",
+      linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
+      from->u8[0], from->u8[1], packetbuf_datalen(), hops);
+}
+
+const static struct conectric_callbacks callbacks = {
+    recv, sent, localbroadcast, netbroadcast, NULL
+};
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(rht_conectric_process, ev, data)
+{
+  static uint8_t * header = NULL;
+  static uint8_t seqno = 0;
+  static unsigned int batt, temp, humid;
+  static unsigned int prev_temp, prev_humid;
+  static uint8_t counter;
+  static float sane;
+  static int dec;
+  static float frac;
+  static struct etimer et;
+  static uint8_t loop;
+
+  static uint8_t payload[4];
+
+  PROCESS_EXITHANDLER(conectric_close(&conectric);)
+
+  PROCESS_BEGIN();
+  SENSORS_ACTIVATE(sht21_sensor);
+
+  conectric_init();
+  conectric_open(&conectric, 132, &callbacks);
+
+  /* Wait until system is completely booted up and ready */
+  etimer_set(&et, CLOCK_SECOND);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+  /* Composing boot status message */
+  batt = adc_sensor.value(ADC_SENSOR_TYPE_VDD);
+  sane = batt * 3 * 1.15 / 2047;
+  dec = sane;
+  frac = sane - dec;
+  payload[0] = CONECTRIC_DEVICE_BROADCAST_BOOT_STATUS;
+  payload[1] = (char)(dec*10)+(char)(frac*10);
+  payload[2] = clock_reset_cause();
+  NETSTACK_MAC.on();
+  conectric_send_signal(&conectric, payload, 3);
+
+  etimer_set(&et, CLOCK_SECOND);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+  PROCESS_WAIT_EVENT();
+
+  deep_sleep_requested = CLOCK_SECOND;
+
+  while(1) {
+
+    PROCESS_WAIT_EVENT();
+
+    /* Battery and temperature acquisition */
+    NETSTACK_MAC.off(0);
+    batt = adc_sensor.value(ADC_SENSOR_TYPE_VDD);
+    sane = batt * 3 * 1.15 / 2047;
+    dec = sane;
+    frac = sane - dec;
+    temp = sht21_sensor.value(SHT21_SENSOR_TEMP_ACQ);
+    deep_sleep_requested = CLOCK_SECOND / 32;
+
+    PROCESS_WAIT_EVENT();
+
+    /* Humidity acquisition */
+    NETSTACK_MAC.off(0);
+    prev_temp = temp;
+    temp = sht21_sensor.value(SHT21_SENSOR_TEMP_RESULT);
+    humid = sht21_sensor.value(SHT21_SENSOR_HUMIDITY_ACQ);
+    deep_sleep_requested = CLOCK_SECOND / 32;
+
+    PROCESS_WAIT_EVENT();
+
+    NETSTACK_MAC.off(0);
+    prev_humid = humid;
+    humid = sht21_sensor.value(SHT21_SENSOR_HUMIDITY_RESULT);
+
+    /* temporary workaround */
+    deep_sleep_requested = CLOCK_SECOND;
+
+    /* Composing RHT sensor message */
+//    memset(message, 0, sizeof(message));
+//    message[0] = RHT_HEADER_SIZE;
+//    message[1] = seqno++;
+//    message[2] = 0;
+//    message[3] = 0;
+//    message[4] = 0;
+//    message[5] = 0;
+//    message[6] = RHT_PAYLOAD_SIZE;
+//    message[7] = CONECTRIC_SENSOR_BROADCAST_RHT;
+//    message[8] = (char)(dec*10)+(char)(frac*10);
+//    message[9] = (char)(temp >> 8);
+//    message[10] = (char)(temp & 0xFC);
+//    message[11] = (char)(humid >> 8);
+//    message[12] = (char)(humid & 0xFC);
+//
+//    loop = CONECTRIC_BURST_NUMBER;
+
+//    /* Log data that will be sent out over the air */
+//    logData[0] = (char)(humid >> 8);
+//    logData[1] = (char)(humid & 0xFC);
+//    logData[2] = (char)(temp >> 8);
+//    logData[3] = (char)(temp & 0xFC);
+//    flashlogging_write4(RIME_RHT_CMP_ID, RHT_SEND, logData);
+
+//    while(loop--) {
+//      packetbuf_copyfrom(message, RHT_HEADER_SIZE + RHT_PAYLOAD_SIZE);
+//
+//      NETSTACK_MAC.on();
+//      broadcast_send(&broadcast);
+//
+//      PROCESS_WAIT_EVENT();
+//
+//      if (loop)
+//        deep_sleep_requested = CLOCK_SECOND / 8 + random_rand() % (CLOCK_SECOND / 8);
+//      else
+//        deep_sleep_requested = RHT_REPORTING_PERIOD;
+//    }
+  }
+
+  SENSORS_DEACTIVATE(sht21_sensor);
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+//PROCESS_THREAD(flash_log_process, ev, data)
+//{
+//  static struct etimer et;
+//
+//  PROCESS_BEGIN();
+//
+//  flashlogging_init();
+//
+//  while (1)
+//  {
+//    etimer_set(&et, LOGGING_REF_TIME_PD);
+//    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+//
+//    flashlogging_write_fullclock(FLASH_LOGGING_CMP_ID, 0);
+//  }
+//
+//  PROCESS_END();
+//}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(serial_in_process, ev, data)
+{
+  static uint8_t * event;
+
+  PROCESS_BEGIN();
+
+  while(1) {
+
+    PROCESS_WAIT_EVENT_UNTIL(ev == serial_line_event_message && data != NULL);
+    PRINTF("Serial_RX: %s (len=%d)\n", (uint8_t *)data, strlen(data));
+    printf("%s\n", (uint8_t *)data);
+
+    event = command_interpreter((uint8_t *)data);
+
+    if (event) {
+      /* do something here */
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+void
+invoke_process_before_sleep(void)
+{
+  process_post_synch(&rht_conectric_process, PROCESS_EVENT_CONTINUE, NULL);
+}
+/*---------------------------------------------------------------------------*/
